9f2381a1df6896d55c739e142bbdad5e9e14aaf2
cadelac <cadelac@github.com>
2018-04-25 06:42:46 +0800

    added argument 'channel' to MessageBlock

--- src/main/java/cadelac/framework/blade/core/dispatch/MessageBlock.java
+++ src/main/java/cadelac/framework/blade/core/dispatch/MessageBlock.java
@@ -4,5 +4,8 @@ import cadelac.framework.blade.core.message.Message;
 
 @FunctionalInterface
 public interface MessageBlock<M extends Message> {
-	public void block(M message_) throws Exception;
+	public void block(
+			String channel_
+			, M message_) 
+					throws Exception;
 }
